b84919ac5d59d77796e7204b412c3cd483e6cc79
Harsh Sharma <harshsharma752136@gmail.com>
2024-10-03 16:53:53 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int num,a,x=0;
+    printf("enter a number to print all the prime numbers till that number: ");
+    scanf("%d",&num);
+    for(int i=2;i<=num;i++){
+        for(a=2;a<i;a++){  // removed  " = " in  a<=i     
+            if(i%a!=0){
+                x--;
+            }
+                    }
+        if (x<=1){
+            printf("%d\n",i);//prime number
+        }
+    } 
+    return 0;
+}
