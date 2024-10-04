7f0975b70b92502fbfb909e5f0d2fbec2ba6545f
Harsh Sharma <harshsharma752136@gmail.com>
2024-10-04 14:13:21 +0530

    Create 03.c

---
+++ 03.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int num,sum;
+    printf("enter a number to print sum till that number: ");
+    scanf("%d",&num);
+    for(int i; i<=num;i++){
+        sum+=i;
+    }
+    printf("the sum is :%d",sum);
+}
