a7756833a6f61ce7fc36e165679149422143139e
Harsh Sharma <harshsharma752136@gmail.com>
2024-10-04 13:56:55 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,17 +1,7 @@
+// To print ASCII values and corresponding characters.
 #include<stdio.h>
 int main(){
-    int num,a,x=0;
-    printf("enter a number to print all the prime numbers till that number: ");
-    scanf("%d",&num);
-    for(int i=2;i<=num;i++){
-        for(a=2;a<i;a++){  // removed  " = " in  a<=i     
-            if(i%a!=0){
-                x--;
-            }
-               }
-        if (x<=1){
-            printf("%d\n",i);//prime number
-        }
-    } 
-    return 0;
+    for(int i=0;i<=255;i++){
+        printf("%d=%c \t",i,i);
+    }
 }
