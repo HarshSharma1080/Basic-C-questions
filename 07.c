98573b2a4d4bd3b99261ee7ecbc1dbb6eb1a3656
Harsh Sharma <harshsharma752136@gmail.com>
2024-10-06 00:42:25 +0530

    Update 07.c

--- 07.c
+++ 07.c
@@ -2,16 +2,16 @@
 #include<stdio.h>
 int main(){
     int even=0,odd=0;
-    char eo;
+    char even_odd;
     printf("even or odd e/o: ");
-    scanf("%c",&eo);
-    if(eo=='e'){
+    scanf("%c",&even_odd);
+    if(even_odd=='E' || even_odd=='e'){
         for(int i=0;i<=100;i+=2){
         even+=i;
     }
     printf("%d",even);
     }
-    else if(eo=='o'){
+    else if(even_odd=='O' || even_odd=='o'){
         for(int i=1;i<=99;i+=2){
         odd+=i;
     }
