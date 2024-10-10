e049f53100889d8261d6c8ee3d6b7043ef62d994
Harsh Sharma <harshsharma752136@gmail.com>
2024-10-11 00:19:28 +0530

    Create 11.c

---
+++ 11.c
@@ -0,0 +1,20 @@
+// To check for Armstrong [magic] number.
+//
+#include<stdio.h>
+int main(){
+  int num,x,a=0,num2;
+  printf("enter a number to check if its armstrong number or not: ");
+  scanf("%d",&num);
+  x=num;
+  do{
+      x=num/10;
+      a++;
+      num=x
+    }
+  }while(x>0);
+for(int i=1;i<=a;i++){
+  num2=num%10;
+  x=num/10
+  num=x
+}
+}
