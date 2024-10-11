4bbbd58acf1698a54fd43eda5ff284e79d0d3ba7
Harsh Sharma <harshsharma752136@gmail.com>
2024-10-11 13:26:08 +0530

    Update 10.c

--- 10.c
+++ 10.c
@@ -1,17 +1,13 @@
 // To find Sum of digits of a number.
 #include<stdio.h>
 int main(){
-  int num,x,a=0,val=0;
+  int num,val=0;
   printf("enter a number to find the sum of its digits: ");
   scanf("%d",&num);
-  do{
-    x=num/10;
-    num=x;
-    a++;
-  }while(x<0);
-  for(int i=1;i<=a;i++){
+  while(num>0){
     val+=num%10;
     num=num/10;
-  }
-  printf("%d",val);
 }
+printf("\nthe sum is %d",val);
+}
+
